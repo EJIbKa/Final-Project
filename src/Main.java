1997107076ac57e3b28fc6f8701c5d94c751ec95
EJIbKa <ejibxkaxupa@gmail.com>
2022-03-19 19:31:59 +0300

    clear

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        //CarService service = new CarService();
-//        factories.CarFactory factory = new factories.CarFactory(
-//                new cars.CarMarksEnum[]{cars.CarMarksEnum.BMW, cars.CarMarksEnum.AUDI, cars.CarMarksEnum.ВАЗ, cars.CarMarksEnum.MERCEDES_BENZ},
-//                new cars.EngineDisplacementEnum[]{cars.EngineDisplacementEnum.MEDIUM_CAPACITY, cars.EngineDisplacementEnum.LARGE_CAPACITY},
-//                new cars.CarColorsEnum[]{cars.CarColorsEnum.BLACK, cars.CarColorsEnum.YELLOW, cars.CarColorsEnum.GREEN},
-//                new cars.WheelSizeEnum[]{cars.WheelSizeEnum.INCH_18, cars.WheelSizeEnum.INCH_19, cars.WheelSizeEnum.INCH_20},
-//                service);
-//        factory.printFactoryOpportunity();
-//        Dealership dealership = new Dealership(service, factory);
-//        cars.Car clientOneCar = new cars.PassengerCar(cars.CarMarksEnum.BMW, 2020, cars.EngineDisplacementEnum.MEDIUM_CAPACITY,
-//                cars.CarColorsEnum.BLACK, cars.WheelSizeEnum.INCH_20, 5, cars.CarOptionsEnum.LEATHER_SEATS, cars.CarOptionsEnum.CLIMATE_CONTROL);
-//        clientOneCar = dealership.addOptionsInCar(clientOneCar, cars.CarOptionsEnum.BACKUP_CAMERA);
-//        cars.Car clientTwoCar = dealership.clientOrder(cars.CarMarksEnum.MERCEDES_BENZ, cars.EngineDisplacementEnum.MEDIUM_CAPACITY,
-//                cars.CarColorsEnum.YELLOW, cars.WheelSizeEnum.INCH_18, cars.CarOptionsEnum.CLIMATE_CONTROL);
-//        System.out.println(clientTwoCar);
-//        cars.Car clientThreeCar = new cars.PassengerCar(cars.CarMarksEnum.AUDI, 2020, cars.EngineDisplacementEnum.LARGE_CAPACITY,
-//                cars.CarColorsEnum.WHITE, cars.WheelSizeEnum.INCH_18, 5, cars.CarOptionsEnum.LEATHER_SEATS, cars.CarOptionsEnum.CLIMATE_CONTROL);
-//        dealership.changeSomethingInCar(clientThreeCar, cars.CarColorsEnum.RED);
     }
 }
