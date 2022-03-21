f8e168723f0b4cf6d859a61e780e17fd104b3d79
EJIbKa <ejibxkaxupa@gmail.com>
2022-03-21 23:45:32 +0300

    refactor storage

--- src/factories/CarStorage.java
+++ src/factories/CarStorage.java
@@ -1,9 +1,11 @@
 package factories;
 
+import Dealership.DealershipApplication;
 import cars.*;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
 
 public class CarStorage {
     private final List<Car> storage;
@@ -13,137 +15,107 @@ public class CarStorage {
     }
 
     public List<Car> getStorage() {
-        return storage;
+        synchronized (storage) {
+            return storage;
+        }
     }
 
-    public Integer searchCarInTheStorage(String[] carArgs) {
-        if (storage.size() > 0) {
-            if (storage.get(0) instanceof TruckCar) {
-                return searchTruckCar(carArgs);
-            }
-            if (storage.get(0) instanceof BusCar) {
-                return searchBusCar(carArgs);
-            }
-            if (storage.get(0) instanceof PassengerCar) {
-                return searchDefaultPassengerCar(carArgs);
-            }
-            if (storage.get(0) instanceof SpecialCar) {
-                return searchSpecialCar(carArgs);
+    public int searchCarInTheStorage(DealershipApplication dealershipApplication) {
+        int indexOfCar;
+        synchronized (storage) {
+            indexOfCar = searchSplitter(dealershipApplication, true);
+            if (indexOfCar < 0) {
+                indexOfCar = searchSplitter(dealershipApplication, false);
             }
         }
+        return indexOfCar;
+    }
+
+    private int searchSplitter(DealershipApplication dealershipApplication, boolean fullSearchOrForChange) {
+        try {
+            if (storage.size() > 0) {
+                switch (dealershipApplication.getCarType()) {
+                    case BUS_CAR -> {
+                        return searchBusCar(dealershipApplication, fullSearchOrForChange);
+                    }
+                    case TRUCK_CAR -> {
+                        return searchTruckCar(dealershipApplication, fullSearchOrForChange);
+                    }
+                    case SPECIAL_CAR -> {
+                        return searchSpecialCar(dealershipApplication, fullSearchOrForChange);
+                    }
+                    case PASSENGER_CAR -> {
+                        return searchPassengerCar(dealershipApplication, fullSearchOrForChange);
+                    }
+                }
+            }
+        } catch (NullPointerException | NoSuchElementException ignored) {}
         return -1;
     }
 
-    public Integer searchCarInTheStorageToChange(String[] carArgs) {
-        if (storage.size() > 0) {
-            if (storage.get(0) instanceof TruckCar) {
-                return searchTruckCar(carArgs);
-            }
-            if (storage.get(0) instanceof BusCar) {
-                return searchBusCar(carArgs);
-            }
-            if (storage.get(0) instanceof PassengerCar) {
-                return searchDefaultPassengerCar(carArgs);
-            }
-            if (storage.get(0) instanceof SpecialCar) {
-                return searchSpecialCar(carArgs);
-            }
+    private Stream<Car> filterForAllCars(Stream<Car> stream,
+                                         DealershipApplication dealershipApplication,
+                                         boolean firstSearch) {
+        if (firstSearch) {
+            return stream.filter( car ->
+                    car.getMark().equals(dealershipApplication.getMark())
+                            && car.getColor().equals(dealershipApplication.getColor())
+                            && car.getWheelSize().equals(dealershipApplication.getWheelSize())
+                            && car.getEngineSize().equals(dealershipApplication.getEngineSize()));
         }
-        return -1;
+        return stream.filter( car -> car.getMark().equals(dealershipApplication.getMark())
+                        && car.getEngineSize().equals(dealershipApplication.getEngineSize()));
     }
 
-    private Integer searchSpecialCar(String[] carArgs) {
-        SpecialCar car;
-        for (int i = 0; i < storage.size(); i++) {
-            car = (SpecialCar) storage.get(i);
-            if (carArgs.length >= 5) {
-                if (car.getMark().equals(CarMarksEnum.valueOf(carArgs[0])) &&
-                        car.getEngineSize().equals(EngineDisplacementEnum.valueOf(carArgs[1])) &&
-                        car.getColor().equals(CarColorsEnum.valueOf(carArgs[2])) &&
-                        car.getWheelSize().equals(WheelSizeEnum.valueOf(carArgs[3])) &&
-                        car.getSpecialCarType().equals(SpecialCarTypeEnum.valueOf(carArgs[4]))) {
-                    return i;
-                }
-            } else {
-                if (car.getMark().equals(CarMarksEnum.valueOf(carArgs[0])) &&
-                        car.getEngineSize().equals(EngineDisplacementEnum.valueOf(carArgs[1])) &&
-                        car.getSpecialCarType().equals(SpecialCarTypeEnum.valueOf(carArgs[2]))) {
-                    return i;
-                }
-            }
-        }
-        return -1;
+    private int searchBusCar(DealershipApplication dealershipApplication, boolean firstSearch)
+            throws NullPointerException, NoSuchElementException {
+        return storage.indexOf(
+                filterForAllCars(storage.stream(), dealershipApplication, firstSearch)
+                        .map(car -> (BusCar) car)
+                        .filter(busCar -> busCar.getBusAppointment().equals(dealershipApplication.getBusAppointment())
+                                && busCar.getOverallDimensions().equals(dealershipApplication.getOverallDimensions()))
+                        .findFirst()
+                        .get()
+        );
     }
 
-    private Integer searchDefaultPassengerCar(String[] carArgs) {
-        PassengerCar car;
-        for (int i = 0; i < storage.size(); i++) {
-            car = (PassengerCar) storage.get(i);
-            if (carArgs.length >= 6) {
-                if (car.getMark().equals(CarMarksEnum.valueOf(carArgs[0])) &&
-                        car.getEngineSize().equals(EngineDisplacementEnum.valueOf(carArgs[1])) &&
-                        car.getColor().equals(CarColorsEnum.valueOf(carArgs[2])) &&
-                        car.getWheelSize().equals(WheelSizeEnum.valueOf(carArgs[3])) &&
-                        car.getPassengerCarBody().equals(PassengerCarBodyEnum.valueOf(carArgs[5]))) {
-                    return i;
-                }
-            } else {
-                if (car.getMark().equals(CarMarksEnum.valueOf(carArgs[0])) &&
-                        car.getEngineSize().equals(EngineDisplacementEnum.valueOf(carArgs[1])) &&
-                        car.getPassengerCarBody().equals(PassengerCarBodyEnum.valueOf(carArgs[3]))) {
-                    return i;
-                }
-            }
-        }
-        return -1;
+    private int searchPassengerCar(DealershipApplication dealershipApplication, boolean firstSearch)
+            throws NullPointerException, NoSuchElementException{
+        return storage.indexOf(
+                filterForAllCars(storage.stream(), dealershipApplication, firstSearch)
+                        .map(car -> (PassengerCar) car)
+                        .filter(passengerCar -> passengerCar.getPassengerCarBody().equals(
+                                dealershipApplication.getPassengerCarBody()
+                        ))
+                        .findFirst()
+                        .get()
+        );
     }
 
-    private Integer searchBusCar(String[] carArgs) {
-        BusCar car;
-        for (int i = 0; i < storage.size(); i++) {
-            car = (BusCar) storage.get(i);
-            if (carArgs.length >= 7) {
-                if (car.getMark().equals(CarMarksEnum.valueOf(carArgs[0])) &&
-                        car.getEngineSize().equals(EngineDisplacementEnum.valueOf(carArgs[1])) &&
-                        car.getColor().equals(CarColorsEnum.valueOf(carArgs[2])) &&
-                        car.getWheelSize().equals(WheelSizeEnum.valueOf(carArgs[3])) &&
-                        car.getOverallDimensions().equals(OverallDimensionsEnum.valueOf(carArgs[5])) &&
-                        car.getBusAppointment().equals(BusAppointmentEnum.valueOf(carArgs[6]))) {
-                    return i;
-                }
-            } else {
-                if (car.getMark().equals(CarMarksEnum.valueOf(carArgs[0])) &&
-                        car.getEngineSize().equals(EngineDisplacementEnum.valueOf(carArgs[1])) &&
-                        car.getOverallDimensions().equals(OverallDimensionsEnum.valueOf(carArgs[3])) &&
-                        car.getBusAppointment().equals(BusAppointmentEnum.valueOf(carArgs[4]))) {
-                    return i;
-                }
-            }
-        }
-        return -1;
+    private int searchSpecialCar(DealershipApplication dealershipApplication, boolean firstSearch)
+            throws NullPointerException, NoSuchElementException{
+        return storage.indexOf(
+                filterForAllCars(storage.stream(), dealershipApplication, firstSearch)
+                        .map(car -> (SpecialCar) car)
+                        .filter(specialCar -> specialCar.getSpecialCarType().equals(
+                                dealershipApplication.getSpecialCarType()
+                        ))
+                        .findFirst()
+                        .get()
+        );
     }
 
-    private Integer searchTruckCar(String[] carArgs) {
-        TruckCar car;
-        for (int i = 0; i < storage.size(); i++) {
-            car = (TruckCar) storage.get(i);
-            if (carArgs.length >= 5) {
-                if (car.getMark().equals(CarMarksEnum.valueOf(carArgs[0])) &&
-                        car.getEngineSize().equals(EngineDisplacementEnum.valueOf(carArgs[1])) &&
-                        car.getColor().equals(CarColorsEnum.valueOf(carArgs[2])) &&
-                        car.getWheelSize().equals(WheelSizeEnum.valueOf(carArgs[3])) &&
-                        car.getCarryingCapacity().equals(CarryingCapacityEnum.valueOf(carArgs[4]))) {
-                    return i;
-                }
-            } else {
-                if (car.getMark().equals(CarMarksEnum.valueOf(carArgs[0])) &&
-                        car.getEngineSize().equals(EngineDisplacementEnum.valueOf(carArgs[1])) &&
-                        car.getCarryingCapacity().equals(CarryingCapacityEnum.valueOf(carArgs[2]))) {
-                    return i;
-                }
-            }
-        }
-        return -1;
+    private int searchTruckCar(DealershipApplication dealershipApplication, boolean firstSearch)
+            throws NullPointerException, NoSuchElementException{
+        return storage.indexOf(
+                filterForAllCars(storage.stream(), dealershipApplication, firstSearch)
+                        .map(car -> (TruckCar) car)
+                        .filter(truckCar -> truckCar.getCarryingCapacity().equals(
+                                dealershipApplication.getCarryingCapacity()
+                        ))
+                        .findFirst()
+                        .get()
+        );
     }
 
     public void addCarToStorage(Car car) {
@@ -152,9 +124,9 @@ public class CarStorage {
         }
     }
 
-    public Car moveCarFromStorageByIndex(Integer index) {
+    public Car moveCarFromStorageByIndex(int index) {
         synchronized (storage) {
-            return storage.remove(index.intValue());
+            return storage.remove(index);
         }
     }
 }
